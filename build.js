8fe2677fa38de3537ca0b51f9ae4736aff011c5b
Steffen Diswal <11992328+spdiswal@users.noreply.github.com>
2025-10-25 17:46:57 +0200

    Store `dist` snapshots in Git
    The new `MAINLINE` environment variable designates the active line of
    Biome versions configured by the files in the `src` directory. This
    allows us to maintain just a single set of configuration files while
    preserving backwards compatibility in the npm package via the snapshots
    of the old build artefacts.
    
    If we ever need to make a change to an obsolete line of Biome versions,
    we can copy-paste the JSON file snapshots back into the JSON files in
    the `src` directory and update the `MAINLINE` environment variable
    accordingly.

--- build.js
+++ build.js
@@ -1,43 +1,71 @@
 import { mkdir, readdir, readFile, writeFile } from "node:fs/promises"
+import process from "node:process"
 
-const versionDirectories = ["2.2"]
-await Promise.all(versionDirectories.map(buildVersion))
+const mainlineVersion = process.env.MAINLINE
+verifyMainlineVersion(mainlineVersion)
 
-async function buildVersion(versionDirectory) {
-	const sourceDirectory = `src/${versionDirectory}`
-	const destinationDirectory = `dist/${versionDirectory}`
-	const reservedFilenames = ["biome.json", "package.json"]
+const sourceDirectory = "src"
+const destinationDirectory = `dist/${mainlineVersion}`
 
-	await mkdir(destinationDirectory, { recursive: true })
+const filenames = await readdir(sourceDirectory)
+const reservedFilenames = ["biome.json", "package.json"]
 
-	const filenames = await readdir(sourceDirectory)
-	await Promise.all(filenames.map(buildFile))
+await mkdir(destinationDirectory, { recursive: true })
+await Promise.all(filenames.map(buildFile))
 
-	async function buildFile(filename) {
-		const destinationFilename = filename.slice(0, -1) // Convert `.jsonc` to `.json`.
+async function buildFile(filename) {
+	const destinationFilename = filename.slice(0, -1) // Convert `.jsonc` to `.json`.
 
-		if (reservedFilenames.includes(destinationFilename)) {
-			throw new Error(`${filename}: Reserved filename.`)
-		}
+	if (reservedFilenames.includes(destinationFilename)) {
+		throw new Error(`${filename}: Reserved filename.`)
+	}
 
-		const sourcePath = `${sourceDirectory}/${filename}`
-		const destinationPath = `${destinationDirectory}/${destinationFilename}`
+	const sourcePath = `${sourceDirectory}/${filename}`
+	const destinationPath = `${destinationDirectory}/${destinationFilename}`
 
-		try {
-			const content = await readFile(sourcePath, "utf8")
-			const output = minifyJson(removeJsonLineComments(content))
-			await writeFile(destinationPath, output, "utf8")
-		} catch (error) {
-			throw new Error(`${filename}: ${error.message}.`)
-		}
+	try {
+		const content = await readFile(sourcePath, "utf8")
+		const jsonContent = removeJsonLineComments(content)
+
+		// Discard the `$schema` field to maintain forwards compatibility with newer patch releases of Biome.
+		const { $schema, ...fieldsToKeep } = JSON.parse(jsonContent)
+		verifyJsonSchemaUrl($schema)
+
+		const minifiedOutput = JSON.stringify(fieldsToKeep)
+		await writeFile(destinationPath, minifiedOutput, "utf8")
+	} catch (error) {
+		throw new Error(`${filename}: ${error.message}.`)
 	}
 }
 
-function removeJsonLineComments(jsonContent) {
-	return jsonContent.replace(/(?<=["}\]0-9e],?\s|\t)\/\/.*$/gm, "")
+function removeJsonLineComments(jsoncContent) {
+	return jsoncContent.replace(/(?<=["}\]0-9e],?\s|\t)\/\/.*$/gm, "")
 }
 
-function minifyJson(jsonContent) {
-	const { $schema: _ignored, ...remainingFields } = JSON.parse(jsonContent)
-	return JSON.stringify(remainingFields)
+function verifyMainlineVersion(version) {
+	if (!version) {
+		throw new Error("Expected the 'MAINLINE' environment variable to be set")
+	}
+	// language=jsregexp
+	if (!version.match("^\\d+\\.\\d+$")) {
+		throw new Error(
+			`Expected the 'MAINLINE' environment variable to be a SemVer <major>.<minor> version number, but got '${version}'`,
+		)
+	}
+}
+
+function verifyJsonSchemaUrl(schemaUrl) {
+	if (typeof schemaUrl !== "string") {
+		throw new Error("Expected the '$schema' field to be a string")
+	}
+	if (
+		!schemaUrl.match(
+			// language=jsregexp
+			`^https://biomejs\\.dev/schemas/${mainlineVersion}\\.\\d+/schema\\.json$`,
+		)
+	) {
+		throw new Error(
+			`Expected the '$schema' field to match the 'MAINLINE' environment variable '${mainlineVersion}', but got '${schemaUrl}'`,
+		)
+	}
 }
